65b8b4267329c58fef5e43f0f551382d1d37bdcc
rohit0985 <rohitsharma4681@gmail.com>
2023-01-15 17:39:02 +0530

    removes error that was 'view breaking in midium screen view'

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import profilePic from "../assets/rohit_kumar_profile_photo.jpg";
 const Home = () => {
   return (
     <Flex
-      flexDirection={{ base: "column", md: "row" }}
+      flexDirection={{ base: "column", md: "row"}}
       alignItems="center"
       justifyContent="center"
       gap="5%"
@@ -20,13 +20,12 @@ const Home = () => {
   borderRadius='20px'
   height={"300px"}
   width={"270px"}
-  // border={"1px"}
-  border="1px"
+  // border="1px"
   p={"2px"}
   src={profilePic}
   alt='Rohit Kumar'
 />
-      {/* <Avatar width="300px" height="300px" border={"0px"} name="Rohit Kumar" src={profilePic}  /> */}
+      {/* <Avatar width="300px" height="300px" border={"0px"} name="Rohit Kumar" src={profilePic} /> */}
       <VStack alignItems={"flex-start"} spacing="5" w={{ base: "90%", md: "60%" }}>
         <Heading>Hi,</Heading>
         <Heading>I am Rohit Kumar</Heading>
--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -4,16 +4,16 @@ import GitHubCalendar from "react-github-calendar";
 
 const Statistics = () => {
   return (
-    <VStack maxW="1000px" m="auto" id="projects" p="4" gap={"6"}>
+    <VStack maxW="100%" m="auto" id="projects" p="4" gap={"6"}>
       <Heading mt={{ base: "16", md: "0" }}>Statistics</Heading>
 
       <Flex>
 
       <Text align="center">
 <Link href="https://github.com/rohit0985">
- <Flex gap={"15px"} alignItems={"flex-start"} flexDirection={{ base: "column", md: "row" }}>
- <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api?username=rohit0985&show_icons=true&theme=buefy&include_all_commits=true&count_private=true"/>
-  <img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=rohit0985&layout=compact&langs_count=8&theme=buefy"/>
+ <Flex gap={"15px"} alignItems={"flex-start"} flexWrap={"wrap"}>
+ <Image  src="https://github-readme-stats-eight-theta.vercel.app/api?username=rohit0985&show_icons=true&theme=buefy&include_all_commits=true&count_private=true"/>
+  <Image  src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=rohit0985&layout=compact&langs_count=8&theme=buefy"/>
  </Flex>
 </Link>
 </Text>
